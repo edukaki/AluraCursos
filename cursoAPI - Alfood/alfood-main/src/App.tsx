ebfdc9dc1402dd17f7f1cbbdb4d9539df6b41d8a
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-15 13:59:05 +0900

    feat: add route to admin page

--- cursoAPI - Alfood/alfood-main/src/App.tsx
+++ cursoAPI - Alfood/alfood-main/src/App.tsx
@@ -1,15 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './paginas/Home';
-import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import { Routes, Route } from "react-router-dom";
+import AdministracaoRestaurante from "./paginas/Administracao/Restaurante";
+import Home from "./paginas/Home";
+import VitrineRestaurantes from "./paginas/VitrineRestaurantes";
 
 function App() {
-
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/restaurantes" element={<VitrineRestaurantes />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/restaurantes" element={<VitrineRestaurantes />} />
+			<Route
+				path="/admin/restaurantes"
+				element={<AdministracaoRestaurante />}
+			/>
+		</Routes>
+	);
 }
 
 export default App;
