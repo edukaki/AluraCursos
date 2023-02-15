698eae57995c361ba822834479cf0e62ea9640b9
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-15 18:04:19 +0900

    feat(novoRestaurante): add function to edit an existing restaurant

--- cursoAPI - Alfood/alfood-main/src/paginas/Administracao/Restaurante/Novo/index.tsx
+++ cursoAPI - Alfood/alfood-main/src/paginas/Administracao/Restaurante/Novo/index.tsx
@@ -1,18 +1,40 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../../interfaces/IRestaurante";
 
 const NovoRestaurante = () => {
-	const url = "http://localhost:8000/api/v2/restaurantes/";
+	const parametros = useParams();
+	const url = `http://localhost:8000/api/v2/restaurantes/${
+		parametros.formId ? parametros.formId : ""
+	}/`;
 	const [nomeRestaurante, setNomeRestaurante] = useState<string>("");
+
+	useEffect(() => {
+		parametros.formId &&
+			axios
+				.get<IRestaurante>(url)
+				.then((resposta) => setNomeRestaurante(resposta.data.nome))
+				.then(() => console.log(nomeRestaurante))
+				.catch((erro) => console.log(erro));
+	}, [parametros]);
+
 	const submeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
 		evento.preventDefault();
-		axios
-			.post(url, {
-				nome: nomeRestaurante,
-			})
-			.then(() => alert("Restaurante cadastrado com sucesso"))
-			.catch((erro) => console.log(erro));
+
+		if (parametros.formId) {
+			axios
+				.put(url, { nome: nomeRestaurante })
+				.then(() => alert("Restaurante atualizado com sucesso"));
+		} else {
+			axios
+				.post(url, {
+					nome: nomeRestaurante,
+				})
+				.then(() => alert("Restaurante cadastrado com sucesso"))
+				.catch((erro) => console.log(erro));
+		}
 	};
 
 	return (
