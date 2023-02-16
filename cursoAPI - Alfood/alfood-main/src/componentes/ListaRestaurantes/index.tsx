4addc5537858781ce8e73d7ae6ee616ea1fe4b8e
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-16 16:48:58 +0900

    feat(listaRestaurantes): create function to search for data

--- cursoAPI - Alfood/alfood-main/src/componentes/ListaRestaurantes/index.tsx
+++ cursoAPI - Alfood/alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -1,18 +1,24 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
 import IPaginacao from "../../interfaces/IPaginacao";
 import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
 
+interface IParametrosBusca {
+	ordering?: string;
+	search?: string;
+}
+
 const ListaRestaurantes = () => {
 	const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
 	const [proximaPagina, setProximaPagina] = useState("");
 	const [anteriorPagina, setAnteriorPagina] = useState("");
+	const [busca, setBusca] = useState("");
 
-	const carregarDados = (url: string) => {
+	const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
 		axios
-			.get<IPaginacao<IRestaurante>>(url)
+			.get<IPaginacao<IRestaurante>>(url, opcoes)
 			.then((resposta) => {
 				setRestaurantes(resposta.data.results);
 				setProximaPagina(resposta.data.next);
@@ -22,6 +28,16 @@ const ListaRestaurantes = () => {
 				console.log(erro);
 			});
 	};
+
+	const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+		evento.preventDefault();
+		const opcoes = {
+			params: {} as IParametrosBusca,
+		};
+		if (busca) opcoes.params.search = busca;
+		carregarDados("http://localhost:8000/api/v1/restaurantes/", opcoes);
+	};
+
 	useEffect(() => {
 		carregarDados("http://localhost:8000/api/v1/restaurantes/");
 	}, []);
@@ -31,6 +47,14 @@ const ListaRestaurantes = () => {
 			<h1>
 				Os restaurantes mais <em>bacanas</em>!
 			</h1>
+			<form onSubmit={buscar}>
+				<input
+					type="text"
+					value={busca}
+					onChange={(evento) => setBusca(evento.target.value)}
+				/>
+				<button type="submit">buscar</button>
+			</form>
 			{restaurantes?.map((item, index) => (
 				<Restaurante restaurante={item} key={index} />
 			))}
