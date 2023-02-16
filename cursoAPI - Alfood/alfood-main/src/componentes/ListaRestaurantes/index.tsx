df7b411d72e31e5e529f751011b22de188085cf9
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-16 16:48:12 +0900

    refactor(listaRestaurantes): create a unique function to get data

--- cursoAPI - Alfood/alfood-main/src/componentes/ListaRestaurantes/index.tsx
+++ cursoAPI - Alfood/alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -10,36 +10,21 @@ const ListaRestaurantes = () => {
 	const [proximaPagina, setProximaPagina] = useState("");
 	const [anteriorPagina, setAnteriorPagina] = useState("");
 
-	useEffect(() => {
-		axios
-			.get<IPaginacao<IRestaurante>>(
-				"http://localhost:8000/api/v1/restaurantes/"
-			)
-			.then((resposta) => {
-				setRestaurantes(resposta.data.results);
-				setAnteriorPagina(resposta.data.previous);
-				setProximaPagina(resposta.data.next);
-			})
-			.catch((erro) => console.log(erro));
-	}, []);
-
-	function verPagina(pagina: string) {
-		const url =
-			pagina === "anterior"
-				? anteriorPagina
-				: pagina === "proxima"
-				? proximaPagina
-				: "";
-
+	const carregarDados = (url: string) => {
 		axios
 			.get<IPaginacao<IRestaurante>>(url)
 			.then((resposta) => {
 				setRestaurantes(resposta.data.results);
-				setAnteriorPagina(resposta.data.previous);
 				setProximaPagina(resposta.data.next);
+				setAnteriorPagina(resposta.data.previous);
 			})
-			.catch((erro) => console.log(erro));
-	}
+			.catch((erro) => {
+				console.log(erro);
+			});
+	};
+	useEffect(() => {
+		carregarDados("http://localhost:8000/api/v1/restaurantes/");
+	}, []);
 
 	return (
 		<section className={style.ListaRestaurantes}>
@@ -50,12 +35,22 @@ const ListaRestaurantes = () => {
 				<Restaurante restaurante={item} key={index} />
 			))}
 
-			{anteriorPagina && (
-				<button onClick={() => verPagina("anterior")}>Pagina Anterior</button>
-			)}
-			{proximaPagina && (
-				<button onClick={() => verPagina("proxima")}>Proxima Pagina</button>
-			)}
+			{
+				<button
+					onClick={() => carregarDados(anteriorPagina)}
+					disabled={!anteriorPagina}
+				>
+					Pagina Anterior
+				</button>
+			}
+			{
+				<button
+					onClick={() => carregarDados(proximaPagina)}
+					disabled={!proximaPagina}
+				>
+					Proxima Pagina
+				</button>
+			}
 		</section>
 	);
 };
