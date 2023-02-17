310c90426c69597824e2b01c904734195b3b003e
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-17 18:06:36 +0900

    feat: add admin navbar to the routes

--- cursoAPI - Alfood/alfood-main/src/App.tsx
+++ cursoAPI - Alfood/alfood-main/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import NavbarAdmin from "./componentes/NavbarAdmin";
 import AdministracaoRestaurante from "./paginas/Administracao/Restaurante";
 import FormularioRestaurante from "./paginas/Administracao/Restaurante/FormularioRestaurante";
 import Home from "./paginas/Home";
@@ -9,18 +10,14 @@ function App() {
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/restaurantes" element={<VitrineRestaurantes />} />
-			<Route
-				path="/admin/restaurantes"
-				element={<AdministracaoRestaurante />}
-			/>
-			<Route
-				path="/admin/restaurantes/novo"
-				element={<FormularioRestaurante />}
-			/>
-			<Route
-				path="/admin/restaurantes/:formId"
-				element={<FormularioRestaurante />}
-			/>
+			<Route path="/admin" element={<NavbarAdmin />}>
+				<Route path="restaurantes" element={<AdministracaoRestaurante />} />
+				<Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+				<Route
+					path="restaurantes/:formId"
+					element={<FormularioRestaurante />}
+				/>
+			</Route>
 		</Routes>
 	);
 }
