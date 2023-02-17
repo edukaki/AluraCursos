201f5caf7715e02470f54e444c44e10ace678b89
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-17 17:50:32 +0900

    feat(administracaoRestaurante): remove unnecessary button

--- cursoAPI - Alfood/alfood-main/src/paginas/Administracao/Restaurante/index.tsx
+++ cursoAPI - Alfood/alfood-main/src/paginas/Administracao/Restaurante/index.tsx
@@ -1,12 +1,16 @@
 import {
+	AppBar,
 	Button,
+	Container,
 	IconButton,
+	Link,
 	Table,
 	TableBody,
 	TableCell,
 	TableContainer,
 	TableHead,
 	TableRow,
+	Typography,
 } from "@mui/material";
 import { DeleteOutline } from "@mui/icons-material";
 import React, { useEffect, useState } from "react";
@@ -37,52 +41,43 @@ const AdministracaoRestaurante = () => {
 	}, []);
 
 	return (
-		<Box>
-			<Button
-				href={`/admin/restaurantes/novo}`}
-				type="button"
-				variant="outlined"
-			>
-				Novo
-			</Button>
-			<TableContainer>
-				<Table>
-					<TableHead>
-						<TableRow>
-							<TableCell>Nome</TableCell>
-							<TableCell>Editar</TableCell>
+		<TableContainer>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>Nome</TableCell>
+						<TableCell>Editar</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{restaurantes.map((restaurante, index) => (
+						<TableRow key={index}>
+							<TableCell>{restaurante.nome}</TableCell>
+							<TableCell>
+								<Button
+									href={`/admin/restaurantes/${restaurante.id}`}
+									type="button"
+									variant="outlined"
+								>
+									Editar
+								</Button>
+								<IconButton
+									type="button"
+									onClick={() => {
+										excluirRestaurante(restaurante);
+									}}
+									aria-label="delete"
+									size="large"
+								>
+									<DeleteOutline fontSize="inherit" />
+								</IconButton>
+							</TableCell>
+							<TableCell></TableCell>
 						</TableRow>
-					</TableHead>
-					<TableBody>
-						{restaurantes.map((restaurante, index) => (
-							<TableRow key={index}>
-								<TableCell>{restaurante.nome}</TableCell>
-								<TableCell>
-									<Button
-										href={`/admin/restaurantes/${restaurante.id}`}
-										type="button"
-										variant="outlined"
-									>
-										Editar
-									</Button>
-									<IconButton
-										type="button"
-										onClick={() => {
-											excluirRestaurante(restaurante);
-										}}
-										aria-label="delete"
-										size="large"
-									>
-										<DeleteOutline fontSize="inherit" />
-									</IconButton>
-								</TableCell>
-								<TableCell></TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</Box>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
 	);
 };
 
