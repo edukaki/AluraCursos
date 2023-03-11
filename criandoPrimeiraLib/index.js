a8b5497a917abed8629875ccc8a8581820eb1ab9
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-11 17:20:53 +0900

    feat(criandoPrimeiraLib): work with try & catch

--- criandoPrimeiraLib/index.js
+++ criandoPrimeiraLib/index.js
@@ -5,17 +5,24 @@ function handleErr(err) {
 	throw new Error(chalk.red(err.code, "error reading file"));
 }
 
-function getFile(path) {
-	fs.promises
-		.readFile(path, "utf-8")
-		.then((text) => {
-			console.log(chalk.green(text));
-		})
-
-		.catch((err) => {
-			handleErr(err);
-		});
+async function getFile(path) {
+	try {
+		const response = await fs.promises.readFile(path, "utf-8");
+		console.log(chalk.green(response));
+	} catch {
+		handleErr(err);
+	}
 }
+// 	fs.promises
+// 		.readFile(path, "utf-8")
+// 		.then((text) => {
+// 			console.log(chalk.green(text));
+// 		})
+
+// 		.catch((err) => {
+// 			handleErr(err);
+// 		});
+// }
 
 // function getFile(path) {
 // 	fs.readFile(path, "utf-8", (err, data) => {
