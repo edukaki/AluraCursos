3b14504c2627145cb0f3697d20f7d9acbea9aa27
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-12 13:34:49 +0900

    feat(criandoPrimeiraLib): give fileProcess the possibility to receive a file or directory path

--- criandoPrimeiraLib/src/cli.js
+++ criandoPrimeiraLib/src/cli.js
@@ -1,11 +1,22 @@
 import chalk from "chalk";
+import fs from "fs";
 import getFile from "./index.js";
 
 const path = process.argv;
 
-async function fileProcess(path) {
-	const result = await getFile(path[2]);
-	console.log(chalk.yellow("List of links"), result);
+async function fileProcess(arg) {
+	const path = arg[2];
+
+	if (fs.lstatSync(path).isFile()) {
+		const result = await getFile(path);
+		console.log(chalk.yellow("List of links"), result);
+	} else if (fs.lstatSync(path).isDirectory()) {
+		const files = await fs.promises.readdir(path);
+		files.map(async (file) => {
+			const result = await getFile(path + file);
+			console.log(chalk.yellow("list of links"), result);
+		});
+	}
 }
 
 fileProcess(path);
