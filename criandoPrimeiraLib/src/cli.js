b1637fdeb4f72e0a4047f325742828cdf6ba0a62
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-12 13:38:39 +0900

    feat(criandoPrimeiraLib): fileProcess treat error

--- criandoPrimeiraLib/src/cli.js
+++ criandoPrimeiraLib/src/cli.js
@@ -7,6 +7,13 @@ const path = process.argv;
 async function fileProcess(arg) {
 	const path = arg[2];
 
+	try {
+		fs.lstatSync(path);
+	} catch (err) {
+		console.log(chalk.red(err));
+		return;
+	}
+
 	if (fs.lstatSync(path).isFile()) {
 		const result = await getFile(path);
 		print(result);
