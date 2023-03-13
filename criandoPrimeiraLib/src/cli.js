95e92ac9532275a59c1e2e783c058bc116d9e67e
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-13 22:07:34 +0900

    feat(criandoPrimeiraLib): treate validate argument

--- criandoPrimeiraLib/src/cli.js
+++ criandoPrimeiraLib/src/cli.js
@@ -1,11 +1,13 @@
 import chalk from "chalk";
 import fs from "fs";
+import validatedList from "./http-validate.js";
 import getFile from "./index.js";
 
 const path = process.argv;
 
 async function fileProcess(arg) {
 	const path = arg[2];
+	const validate = arg[3] === "--validate";
 
 	try {
 		fs.lstatSync(path);
@@ -16,22 +18,30 @@ async function fileProcess(arg) {
 
 	if (fs.lstatSync(path).isFile()) {
 		const result = await getFile(path);
-		print(result);
+		print(validate, result);
 	} else if (fs.lstatSync(path).isDirectory()) {
 		const files = await fs.promises.readdir(path);
 		files.map(async (file) => {
 			const result = await getFile(path + file);
-			print(result, file);
+			print(validate, result, file);
 		});
 	}
 }
 
-function print(result, file = "") {
-	console.log(
-		file && chalk.black.bgGreen(file + ":"),
-		chalk.yellow("list of links"),
-		result
-	);
+function print(validate, result, file = "") {
+	if (validate) {
+		console.log(
+			file && chalk.black.bgGreen(file + ":"),
+			chalk.yellow("validated links"),
+			validatedList(result)
+		);
+	} else {
+		console.log(
+			file && chalk.black.bgGreen(file + ":"),
+			chalk.yellow("list of links"),
+			result
+		);
+	}
 }
 
 fileProcess(path);
