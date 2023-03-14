d55782fc72f07127d675db7233b902a5fad0b74e
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-14 18:01:26 +0900

    feat(criandoPrimeiraLib): validate links

--- criandoPrimeiraLib/src/http-validate.js
+++ criandoPrimeiraLib/src/http-validate.js
@@ -1,3 +1,19 @@
+function getLinksFromArray(arrLinks) {
+	return arrLinks.map((objlink) => Object.values(objlink).join());
+}
+
+function testLinks(arrLinks) {
+	console.log(arrLinks);
+	arrLinks.forEach((link) => {
+		fetch(link)
+			.then((response) => {
+				console.log(response.status);
+			})
+			.catch((error) => {});
+	});
+}
+
 export default function validatedList(list) {
-	return "enter the function validatedList";
+	const arrLinks = getLinksFromArray(list);
+	return testLinks(arrLinks);
 }
