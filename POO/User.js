96c7cb85870e671f5a933cc0230d0568fc49983c
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-15 13:04:39 +0900

    feat(POO): create admin class that extends user

---
+++ POO/User.js
@@ -0,0 +1,13 @@
+export default class User {
+	constructor(name, email, bornDate, role, active = true) {
+		this.name = name;
+		this.email = email;
+		this.bornDate = bornDate;
+		this.role = role || "student";
+		this.active = active;
+	}
+
+	getInformation() {
+		return `Name: ${this.name} Email: ${this.email} Born Date: ${this.bornDate} Role: ${this.role}`;
+	}
+}
