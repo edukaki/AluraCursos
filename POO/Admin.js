c1a522ba872c10211bd58427d0f16efed1eaef99
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-15 13:55:14 +0900

    feat(POO): create private method

--- POO/Admin.js
+++ POO/Admin.js
@@ -5,8 +5,13 @@ class Admin extends User {
 		super(name, email, bornDate, role, active);
 		this.role = "admin";
 	}
+
+	createCourse(course, vacancies) {
+		return `${course} course created with ${vacancies} vacancies`;
+	}
 }
 
 const newAdmin = new Admin("Edu", "edu@email.com", "1991-04-01");
 
 console.log(newAdmin.getInformation());
+console.log(newAdmin.createCourse("JS", 20));
