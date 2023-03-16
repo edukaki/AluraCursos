7b3ec644e4a4b3921a1acd2570b7bb8e06b552a5
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-16 21:37:41 +0900

    feat(POO): working with getters and setters

--- POO/User.js
+++ POO/User.js
@@ -1,29 +1,80 @@
 export default class User {
 	#name;
+	#lastName;
 	#email;
 	#bornDate;
 	#role;
 	#active;
-	constructor(name, email, bornDate, role, active = true) {
+	constructor(name, lastName, email, bornDate, role, active = true) {
 		this.#name = name;
+		this.#lastName = lastName;
 		this.#email = email;
 		this.#bornDate = bornDate;
 		this.#role = role || "student";
 		this.#active = active;
 	}
 
-	#mountObjectUser() {
-		return {
-			name: this.#name,
-			email: this.#email,
-			bornDate: this.#bornDate,
-			role: this.#role,
-			active: this.#active,
-		};
+	get name() {
+		return this.#name;
 	}
 
+	get lastName() {
+		return this.#lastName;
+	}
+
+	get email() {
+		return this.#email;
+	}
+
+	get bornDate() {
+		return this.#bornDate;
+	}
+
+	get role() {
+		return this.#role;
+	}
+
+	get active() {
+		return this.#active;
+	}
+
+	set name(name) {
+		let [firstName, ...lastName] = name.split(" ");
+		if (name.length < 3) {
+			throw new Error("Name must have at least 3 characters");
+		}
+
+		this.#name = firstName;
+		this.#lastName = lastName.join(" ");
+	}
+
+	set email(email) {
+		this.#email = email;
+	}
+
+	set bornDate(bornDate) {
+		this.#bornDate = bornDate;
+	}
+
+	set role(role) {
+		this.#role = role;
+	}
+
+	set active(active) {
+		this.#active = active;
+	}
+
+	// #mountObjectUser() {
+	// 	return {
+	// 		name: this.#name,
+	// 		email: this.#email,
+	// 		bornDate: this.#bornDate,
+	// 		role: this.#role,
+	// 		active: this.#active,
+	// 	};
+	// }
+
 	getInformation() {
-		const objUser = this.#mountObjectUser();
-		return `Name: ${objUser.name} Email: ${objUser.email} Born Date: ${objUser.bornDate} Role: ${objUser.role}`;
+		return `Name: ${this.name} LastName: ${this.lastName} Email: ${this.email} Born Date: ${this.bornDate} Role: ${this.role}`;
 	}
 }
