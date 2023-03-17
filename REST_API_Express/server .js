70b8133cf5d4281104333a86b65a67127a80a4ce
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-17 22:01:54 +0900

    feat(REST_API_Express): create routes

--- REST_API_Express/server .js
+++ REST_API_Express/server .js
@@ -1,9 +1,16 @@
 import http from "http";
 
 const port = 3000;
+
+const routes = {
+	"/": "Hello World",
+	"/about": "About Us",
+	"/contact": "Contact Us",
+};
+
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { "content-type": "text/plain" });
-	res.end("Hello World\n");
+	req.url in routes ? res.end(routes[req.url]) : res.end("404 Not Found");
 });
 
 server.listen(port, () => {
