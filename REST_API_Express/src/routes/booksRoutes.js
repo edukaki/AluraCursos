24a57bcc6563f4418e8100cec280c6bf7d4dedc7
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-19 21:32:16 +0900

    feat(REST_API_Express): add new routes to update and delete

--- REST_API_Express/src/routes/booksRoutes.js
+++ REST_API_Express/src/routes/booksRoutes.js
@@ -6,5 +6,7 @@ const router = Express.Router();
 router.get("/books", BookController.getBooks);
 router.get("/books/:_id", BookController.getBooksById);
 router.post("/books/", BookController.insertNewBook);
+router.put("/books/:_id", BookController.updateBook);
+router.delete("/books/:_id", BookController.deleteBook);
 
 export default router;
