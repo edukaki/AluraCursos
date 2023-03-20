dee57a2dd0d21f3b9802a588b1225430adfdf44a
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-20 15:45:38 +0900

    refactor(REST_API_Express/router): ensure better readability

--- REST_API_Express/src/routes/booksRoutes.js
+++ REST_API_Express/src/routes/booksRoutes.js
@@ -3,10 +3,11 @@ import BookController from "../controllers/booksController.js";
 
 const router = Express.Router();
 
-router.get("/books", BookController.getBooks);
-router.get("/books/:_id", BookController.getBooksById);
-router.post("/books/", BookController.insertNewBook);
-router.put("/books/:_id", BookController.updateBook);
-router.delete("/books/:_id", BookController.deleteBook);
+router
+	.get("/books", BookController.getBooks)
+	.get("/books/:_id", BookController.getBooksById)
+	.post("/books/", BookController.insertNewBook)
+	.put("/books/:_id", BookController.updateBook)
+	.delete("/books/:_id", BookController.deleteBook);
 
 export default router;
