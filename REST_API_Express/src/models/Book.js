74afc01462fee51b6f641f65e0886bcd5ff3b856
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-20 19:39:47 +0900

    feat(REST_API_Express/models): include relational concepts between books and author schemas

--- REST_API_Express/src/models/Book.js
+++ REST_API_Express/src/models/Book.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 const bookSchema = new mongoose.Schema({
 	id: { type: String },
 	title: { type: String, required: true },
-	author: { type: String, required: true },
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "authors",
+		required: true,
+	},
+	publisher: { type: String },
 });
 
 const books = mongoose.model("books", bookSchema);
